e34c85bdea04f201a34511f384197ef72f4cb0e1
Divya <divya_dia@live.com>
2019-02-15 16:30:32 +0530

    Update create_jira.py

--- actions/create_jira.py
+++ actions/create_jira.py
@@ -6,9 +6,8 @@ class jira(Action):
     def run(self, summary):
         jira_url = self.config['jira_url']
         username = self.config['jira_username']
-#        password = self.config['password']
         password = self.config['jira_password']
         self.jira_client = JIRA(jira_url, basic_auth=(username, password))
-        jiraNum = self.jira_client.create_issue(project = "TS", summary = summary, description = summary, issuetype= "Story")
+        jiraNum = self.jira_client.create_issue(project = project, summary = summary, description = summary, issuetype= issuetype)
         msg = "Jira " + str(jiraNum) + " is created"
         return msg
